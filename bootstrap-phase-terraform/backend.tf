a2da7cff370f88298b5439c8f7df60c7ef95ec6a
Nalla06 <lakshmi.rajyam06@gmail.com>
2025-05-20 11:15:55 +0200

    Images added

--- bootstrap-phase-terraform/backend.tf
+++ bootstrap-phase-terraform/backend.tf
@@ -5,12 +5,16 @@ provider "aws" {
 resource "aws_s3_bucket" "tf_state" {
   bucket = "my-eks-bucket6"
 
-  versioning {
-    enabled = true
-  }
-
   tags = {
     Name = "Terraform State Bucket"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning_tf_state" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  versioning_configuration {
+    status = "Enabled"
   }
 }
 
